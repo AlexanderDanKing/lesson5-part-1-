a4b544b59b02d1be92d1db51fbf279684e4b0b06
Alexander Dankov <dankunchik@gmail.com>
2019-04-28 00:10:24 +0500

    Первая часть домашнего задания, урок №5

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let ul = document.querySelector(".menu"),
+    li = document.createElement("li"),
+    body = document.querySelector("body"),
+    title = document.getElementById("title"),
+    column = document.querySelectorAll(".column"),
+    adv = document.querySelector(".adv"),
+    ans = document.getElementById("prompt");
+
+li.classList.add("menu-item");
+ul.appendChild(li);
+body.style.background = "url(img/apple_true.jpg) center no-repeat";
+title.innerHTML = "Мы продаем только подлинную технику Apple";
+column[1].removeChild(adv);
+ans.innerHTML = prompt("Как Вы относитесь к технике Apple?");
